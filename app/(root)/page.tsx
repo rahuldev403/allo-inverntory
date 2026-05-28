a12b77888f0bde5bfdd5e5c42d067089ee51ca25
Rahul Swain <rahuls.403.dev@gmail.com>
2026-05-28 13:44:45 +0530

    refactor: restructure layout in Page component for improved readability

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -15,28 +15,30 @@ const Page = async ({
   const books = bookResults.success ? (bookResults.data ?? []) : [];
 
   return (
-    <main className="wrapper container">
+    <main className="container">
       <Navbar />
       <Hero />
 
-      <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-5 mb-10">
-        <h2 className="text-3xl font-serif font-bold text-[#212a3b]">
-          Recent Books
-        </h2>
-        <Search />
-      </div>
+      <section className="wrapper">
+        <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-5 mb-10">
+          <h2 className="text-3xl font-serif font-bold text-[#212a3b]">
+            Recent Books
+          </h2>
+          <Search />
+        </div>
 
-      <div className="library-books-grid">
-        {books.map((book) => (
-          <BookCard
-            key={book._id}
-            title={book.title}
-            author={book.author}
-            coverURL={book.coverURL}
-            slug={book.slug}
-          />
-        ))}
-      </div>
+        <div className="library-books-grid">
+          {books.map((book) => (
+            <BookCard
+              key={book._id}
+              title={book.title}
+              author={book.author}
+              coverURL={book.coverURL}
+              slug={book.slug}
+            />
+          ))}
+        </div>
+      </section>
     </main>
   );
 };
