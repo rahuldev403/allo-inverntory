036cadc802313d7b7dc8e8168c0e93b4c4b44043
Rahul Swain <rahuls.403.dev@gmail.com>
2026-05-28 11:38:03 +0530

    implement database setup and much more

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,42 @@
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import React from "react";
+import BookCard from "@/components/BookCard";
+import { getAllBooks } from "@/lib/actions/book.actions";
+import Search from "@/components/Search";
+import Hero from "@/components/Hero.tsx";
 
-const page = () => {
+const Page = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) => {
+  const { query } = await searchParams;
+
+  const bookResults = await getAllBooks(query);
+  const books = bookResults.success ? (bookResults.data ?? []) : [];
+
   return (
-    <div>
-      <Navbar />
+    <main className="wrapper container">
       <Hero />
-    </div>
+
+      <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-5 mb-10">
+        <h2 className="text-3xl font-serif font-bold text-[#212a3b]">
+          Recent Books
+        </h2>
+        <Search />
+      </div>
+
+      <div className="library-books-grid">
+        {books.map((book) => (
+          <BookCard
+            key={book._id}
+            title={book.title}
+            author={book.author}
+            coverURL={book.coverURL}
+            slug={book.slug}
+          />
+        ))}
+      </div>
+    </main>
   );
 };
 
-export default page;
+export default Page;
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -86,17 +86,6 @@ const Hero = () => {
           </div>
         </div>
       </div>
-      <div className="library-books-grid">
-        {sampleBooks.map((book) => (
-          <BookCard
-            key={book._id}
-            title={book.title}
-            author={book.author}
-            coverURL={book.coverURL}
-            slug={book.slug}
-          />
-        ))}
-      </div>
     </section>
   );
 };
